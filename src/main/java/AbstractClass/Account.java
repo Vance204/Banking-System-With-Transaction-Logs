bf2583fb6cc2e13590372f7ee87f14834ce43302
kahel <iamsammy70@gmail.com>
2025-09-23 13:05:47 +0800

    First Commit

---
+++ src/main/java/AbstractClass/Account.java
@@ -0,0 +1,14 @@
+package AbstractClass;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ORTEGA
+ */
+public abstract class Account {
+    
+}
